91e69a66bc619512ec3e7bea579e5f9fdb7c1f34
Krishnendu M R <130251084+KrishnenduMR@users.noreply.github.com>
2025-03-27 19:26:47 +0530

    Update cron_job.py

--- notebooks/cron_job.py
+++ notebooks/cron_job.py
@@ -187,6 +187,6 @@ if __name__ == "__main__":
     for station, station_location in stations:
         setData(station, station_location, logger, TOKEN)
 
-    # if datetime.utcnow().hour == 20:
-    writeData(alandur_OUTPUT, alandur_DAILY_AQI)
-    retrain_model(ORDER, SEASONAL_ORDER, alandur_DAILY_AQI)
+    if datetime.utcnow().hour == 20:
+        writeData(alandur_OUTPUT, alandur_DAILY_AQI)
+        retrain_model(ORDER, SEASONAL_ORDER, alandur_DAILY_AQI)
